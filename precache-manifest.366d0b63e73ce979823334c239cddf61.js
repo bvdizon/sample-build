2a50f0838acf715e4c024b562dab041b0e571750
briandizon <bvdizon@gmail.com>
2020-10-05 10:08:43 +0800

    first commit

---
+++ precache-manifest.366d0b63e73ce979823334c239cddf61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcbb1d967ef43de5144a334c9fddebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d9be30d1e794f36e439",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "8d9be30d1e794f36e439",
+    "url": "/static/js/main.1169c86a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
